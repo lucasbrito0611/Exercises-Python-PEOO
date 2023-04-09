05e197d22a93ac394385f081753200e76b8f4ec4
lucasbrito0611 <122239789+lucasbrito0611@users.noreply.github.com>
2023-04-09 15:34:31 -0300

    Create Ex03.py

---
+++ Lista 2/Ex03.py
@@ -0,0 +1,9 @@
+print('Digite a nota do primeiro bimestre da disciplina:')
+nota_1 = int(input())
+
+print('Digite a nota do segundo bimestre da disciplina:')
+nota_2 = int(input())
+
+media = (nota_1 * 2 + nota_2 * 3) / 5
+
+print(f'Media Parcial = {media:.0f}')
