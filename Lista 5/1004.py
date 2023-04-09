e907e3fb3e84f6a9448760063b1622fa692724cb
lucasbrito0611 <122239789+lucasbrito0611@users.noreply.github.com>
2023-04-09 16:03:54 -0300

    Create 1004.py

---
+++ Lista 5/1004.py
@@ -0,0 +1,15 @@
+a, b = input(). split()
+a = int(a)
+b = int(b)
+
+if b > a:
+  if b % a == 0:
+   print('Sao Multiplos')
+  else:
+    print('Nao sao Multiplos')
+
+elif a > b:
+  if a % b == 0:
+    print('Sao Multiplos')
+  else:
+    print('Nao sao Multiplos')
