a2e2bfa82fafb9c0af64ef5647a22b948c80d68e
lucasbrito0611 <122239789+lucasbrito0611@users.noreply.github.com>
2023-04-09 16:05:42 -0300

    Create 1050.py

---
+++ Lista 5/1050.py
@@ -0,0 +1,30 @@
+x = int(input())
+
+a = 61
+b = 71
+c = 11
+d = 21
+e = 32
+f = 19
+g = 27
+h = 31
+
+if x == a:
+ print('Brasilia')
+elif x == b:
+ print('Salvador')
+elif x == c:
+ print('Sao Paulo')
+elif x == d:
+ print('Rio de Janeiro')
+elif x == e:
+ print('Juiz de Fora')
+elif x == f:
+ print('Campinas')
+elif x == g:
+ print('Vitoria')
+elif x == h:
+ print('Belo Horizonte')
+
+else:
+ print('DDD nao cadastrado')
